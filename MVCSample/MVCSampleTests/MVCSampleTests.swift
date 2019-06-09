652302b878a18f975d1224578d659b8fb199e61f
iganin <hironobu.iga.developer@gmail.com>
2019-06-09 14:50:29 +0900

    [add]MVC用サンプルプロジェクト追加

---
+++ MVCSample/MVCSampleTests/MVCSampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  MVCSampleTests.swift
+//  MVCSampleTests
+//
+//  Created by HironobuIga on 2019/06/09.
+//  Copyright © 2019 iganin. All rights reserved.
+//
+
+import XCTest
+@testable import MVCSample
+
+class MVCSampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
